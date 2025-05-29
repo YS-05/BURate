28eeb15f6f0aff414b3890fec04884a590c427ec
YS-05 <yash23sharma05@gmail.com>
2025-05-29 18:43:57 -0500

    first commit

---
+++ CourseGraderBackend/src/main/java/com/coursegrade/CourseGraderBackend/CourseGraderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coursegrade.CourseGraderBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseGraderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseGraderBackendApplication.class, args);
+	}
+
+}
---
+++ CourseGraderBackend/src/test/java/com/coursegrade/CourseGraderBackend/CourseGraderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursegrade.CourseGraderBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseGraderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
