8bd5a69093303b21926b503262395338193414d1
YS-05 <yash23sharma05@gmail.com>
2025-06-07 14:33:09 -0500

    Making scraping logic modular and adding webpage info for each college

--- CourseGraderBackend/src/main/java/com/coursegrade/CourseGraderBackend/service/WebScraperService.java
+++ CourseGraderBackend/src/main/java/com/coursegrade/CourseGraderBackend/service/WebScraperService.java
@@ -13,11 +13,36 @@ import java.util.List;
 public class WebScraperService {
 
     @PostConstruct
-    public void casCourseNames() {
+    public void wrapperScrape() {
         List<String> courseUrls = new ArrayList<>();
-        int numPages = 153;
-        String baseUrl = "https://www.bu.edu/academics/cas/courses/";
-        for (int i = 1; i <= 3; i++) { // Replace with numPages when not testing
+
+        courseNames(courseUrls, "https://www.bu.edu/academics/cas/courses/", 153, "CAS"); // For College of Arts & Sciences
+        courseNames(courseUrls, "https://www.bu.edu/academics/khc/courses/", 3, "KHC"); // For Arvind & Chandan Nandlal Kilachand Honors College
+        courseNames(courseUrls, "https://www.bu.edu/academics/hub/courses/", 1, "HUB"); // For BU Hub
+        courseNames(courseUrls, "https://www.bu.edu/academics/camed/courses/", 8, "MED"); // For Chobanian & Avedisian School of Medicine
+        courseNames(courseUrls, "https://www.bu.edu/academics/com/courses/", 14, "COM"); // For College of Communications
+        courseNames(courseUrls, "https://www.bu.edu/academics/eng/courses/", 20, "ENG"); // For College of Engineering
+        courseNames(courseUrls, "https://www.bu.edu/academics/cfa/courses/", 39, "CFA"); // For College of Fine Arts
+        courseNames(courseUrls, "https://www.bu.edu/academics/cgs/courses/", 2, "CGS"); // For College of General Studies
+        courseNames(courseUrls, "https://www.bu.edu/academics/cds/courses/", 3, "CDS"); // For Faculty of Computing & Data Sciences
+        courseNames(courseUrls, "https://www.bu.edu/academics/gms/courses/", 23, "GMS"); // For Graduate Medical Sciences
+        courseNames(courseUrls, "https://www.bu.edu/academics/grs/courses/", 59, "CAS"); // For Graduate School of Arts & Sciences
+        courseNames(courseUrls, "https://www.bu.edu/academics/sdm/courses/", 20, "SDM"); // For Henry M. Goldman School of Dental Medicine
+        courseNames(courseUrls, "https://www.bu.edu/academics/met/courses/", 28, "GMS"); // For Metropolitan College & Extended Education
+        courseNames(courseUrls, "https://www.bu.edu/academics/questrom/courses/", 21, "QST"); // For Questrom School of Business
+        courseNames(courseUrls, "https://www.bu.edu/academics/sar/courses/", 15, "SAR"); // For Sargent College of Health & Rehabilitation Sciences
+        courseNames(courseUrls, "https://www.bu.edu/academics/sha/courses/", 4, "SHA"); // For School of Hospitality Administration
+        courseNames(courseUrls, "https://www.bu.edu/academics/law/courses/", 17, "LAW"); // For School of Law
+        courseNames(courseUrls, "https://www.bu.edu/academics/sph/courses/", 13, "SPH"); // For School of Public Health
+        courseNames(courseUrls, "https://www.bu.edu/academics/ssw/courses/", 4, "SSW"); // For School of Social Work
+        courseNames(courseUrls, "https://www.bu.edu/academics/sth/courses/", 12, "STH"); // For School of Theology
+        courseNames(courseUrls, "https://www.bu.edu/academics/wheelock/courses/", 26, "WED"); // For Wheelock College of Education & Human Development
+
+        courseHubsFulfilled(courseUrls);
+    }
+
+    public void courseNames(List<String> courseUrls, String baseUrl, int numPages, String college) {
+        for (int i = 1; i <= 1; i++) { // Replace with numPages when not testing
             try {
                 Document doc = Jsoup.connect(baseUrl + Integer.toString(i))
                         .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
@@ -26,9 +51,10 @@ public class WebScraperService {
                 doc.select("a").forEach(element -> {
                     String title = element.text();
                     String href = element.attr("href");
-                    if (title.contains("CAS ") && href.contains("/courses/")) {
+                    if (title.contains(college) && href.contains("/courses/")) {
                         courseUrls.add("https://www.bu.edu" + href);
                         System.out.println(title);
+                        courseDetails(title);
                     }
                 });
             } catch (IOException e) {
@@ -39,12 +65,9 @@ public class WebScraperService {
         }
         System.out.println("All course names scraped");
         System.out.println("Number of courses: " +courseUrls.size());
-        List<String> testUrls = courseUrls.subList(0, Math.min(5, courseUrls.size()));
-        System.out.println("Testing with first " + testUrls.size() + " courses...");
-        casCourseHubsFulfilled(testUrls);
     }
 
-    public void casCourseHubsFulfilled(List<String> courseUrls) {
+    public void courseHubsFulfilled(List<String> courseUrls) {
         for (String courseUrl : courseUrls) {
             try {
                 Document doc = Jsoup.connect(courseUrl)
@@ -66,10 +89,20 @@ public class WebScraperService {
 
     public void threadSleeper() {
         try {
-            Thread.sleep(500); // .5 second delay to prevent overload
+            Thread.sleep(1000); // 1-second delay to prevent overload
         } catch (InterruptedException e) {
             System.out.println("Sleep interrupted: " + e.getMessage());
         }
+    }
+
+    public void courseDetails(String title) {
+        String[] sub = title.split(" ");
+        System.out.println("College: " + sub[0]);
+        System.out.println("Department: " + sub[1]);
+        String[] preCol = sub[2].split(":");
+        System.out.println("Course number: " + preCol[0]);
+        String[] byColon = title.split(": ");
+        System.out.println("Course name: " + byColon[1]);
     }
 
 }
